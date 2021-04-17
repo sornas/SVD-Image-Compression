b2d800e838363aba8cd4999d00d870390ddbfca5
Gustav Sörnäs <gustav@sornas.net>
2021-04-17 23:43:02 +0200

    logger

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
+use log::*;
 use nalgebra::base::DMatrix;
 use std::path::Path;
 use std::fs::File;
 
 fn main() {
-    println!("Decoding image");
+    setup_logger();
+
+    info!("Decoding image");
     let decoder = png::Decoder::new(File::open("res/cows.png").unwrap());
     let (info, mut reader) = decoder.read_info().unwrap();
-    println!("{:?}", info);
+    debug!("{:?}", info);
 
-    println!("Creating buffer");
+    info!("Creating buffer");
     let mut buf = vec![0; info.buffer_size()];
 
-    println!("Reading frame");
+    info!("Reading frame");
     reader.next_frame(&mut buf).unwrap();
 
-    println!("Creating matrix");
+    info!("Creating matrix");
     let mut mat = DMatrix::from_element(info.width as usize, info.height as usize, [0, 0, 0]);
     let mut pixels = buf.chunks_exact(3);
     for y in 0..info.height {
@@ -24,21 +27,54 @@ fn main() {
         }
     }
 
-    println!("Creating output file");
+    info!("Creating output file");
     let path = Path::new("out.png");
     let file = File::create(path).unwrap();
     let w = std::io::BufWriter::new(file);
 
-    println!("Preparing encoder");
+    info!("Preparing encoder");
     let mut encoder = png::Encoder::new(w, info.width, info.height);
     encoder.set_color(info.color_type);
     encoder.set_depth(info.bit_depth);
 
-    println!("Encoding and saving image");
+    info!("Encoding and saving image");
     let mut writer = encoder.write_header().unwrap();
     let mut data = Vec::new();
     for pixel in mat.into_iter() {
         data.extend(pixel.iter());
     }
     writer.write_image_data(&data).unwrap();
+}
+
+fn setup_logger() {
+    use colored::*;
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            let message = message.to_string();
+            out.finish(format_args!(
+                "{} {} {}:{}{}{}",
+                match record.level() {
+                    Level::Error => "ERROR".red(),
+                    Level::Warn => "WARN ".yellow(),
+                    Level::Info => "INFO ".normal(),
+                    Level::Debug => "DEBUG".green(),
+                    Level::Trace => "TRACE".normal(),
+                },
+                // chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S%.6f]"),
+                chrono::Local::now().format("[%H:%M:%S%.6f]"),
+                record.file().unwrap(),
+                record.line().unwrap(),
+                if message.chars().any(|e| e == '\n') {
+                    "\n"
+                } else {
+                    " "
+                },
+                message
+            ))
+        })
+        .level(LevelFilter::Debug)
+        .chain(std::io::stderr())
+        .apply()
+        .unwrap();
 }
